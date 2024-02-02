e807dc31cfeab56aec37fd20e24aa88a047723ca
ATHARV DALAL <sangamdalal24@gmail.com>
2024-02-02 19:33:28 +0530

    Update User APIs

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,73 @@
 //GEt USER INFO
 
-const getUserController = async(req,res)=>{
-         res.status(200).send("User data")
+import { User } from "../models/userModel.js";
+
+export const getUserController = async(req,res)=>{
+         try {
+            //find User
+      const user = await User.findById({_id:req.body.id})
+      //validation
+
+      if (!user) {
+        return res.status(404).send({
+               success:false,
+               msg: 'User Not Found',
+               error
+        })
+        
+      }
+        //hide Password
+        user.password = undefined;
+        res.status(200).send({
+            success: true,
+            msg: "User data get Successfully",
+            user
+        })
+
+         } catch (error) {
+            console.log(error);
+            res.statust(500).send({
+               success:false,
+               msg: 'Error in Get User API',
+               error
+            })
+         }
 }
 
-export default getUserController
+
+
+ export const updateUserController = async(req,res)=>{
+try {
+
+    const user = await User.findById({_id: req.body.id})
+    if (!user) {
+        return res.status(404).send({
+               success:false,
+               msg: 'User Not Found',
+               error
+        })
+        
+    }
+    //User Update
+    const {username, address, phone } = req.body;
+    if (username) user.username = username;
+    if (address) user.address = address;
+    if (phone) user.phone = phone;
+
+    //save
+    await user.save()
+    res.status(200).send({
+        success:true,
+      msg: 'User Update Successfully',
+    
+    })
+
+} catch (error) {
+    console.log(error);
+    res.status(500).send({
+        success:false,
+        msg: 'Error in Update User API',
+        error})
+}
+}
+
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import getUserController from '../controllers/userController.js'
 import authMiddleware from '../middleware/authMiddleware.js'
+import { getUserController, updateUserController } from '../controllers/userController.js'
 
 const router = express.Router()
 
 //
 
 router.get('/getuser', authMiddleware, getUserController)
+
+router.put('/updateuser',authMiddleware, updateUserController)
 export default router
