468b3acabf22e6c11e5384f6e381e102420eaca4
ATHARV DALAL <sangamdalal24@gmail.com>
2024-02-02 16:50:58 +0530

    Auth Middleware For Checkeing User Token

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+
+
+
+
+
+ const authMiddleware =async(req,res,next)=> {
+      
+    try {
+        const token = req.headers["authorization"].split(" ")[1]
+        jwt.verify(token, process.env.JWT_SECRET,(err,decode)=>{
+            if (err) {
+                return res.status(401).send({
+                    success:false,
+                    msg:"Un-Authorize User"
+                })
+            }else{
+                req.body.id = decode.id;
+                next()
+            }
+        })
+    } catch (error) {
+       console.log(error);
+       res.status(500).send({
+        success:false,
+        msg:'Error im auth API'
+       })
+    }
+}
+
+export default authMiddleware;
