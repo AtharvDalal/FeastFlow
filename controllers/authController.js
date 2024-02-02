4429fcfac8c4fe94b76b290547233f2839f0bc7e
ATHARV DALAL <sangamdalal24@gmail.com>
2024-02-02 16:49:52 +0530

    Auth With Password Hashing

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,7 @@
 import { User } from "../models/userModel.js";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
 
 const registerController = async (req, res) => {
   try {
@@ -20,10 +23,13 @@ const registerController = async (req, res) => {
             msg: 'Email Already Existed'
         })       
     }
+    //Hashing Passowrd
+    var salt = bcryptjs.genSaltSync(10)
+    const hashPassword = await bcryptjs.hash(password, salt)
 
     //create New User
 
-    const user = await User.create({username, email, password,address,phone})
+    const user = await User.create({username, email, password: hashPassword,address,phone})
     res.status(201).send({
       success: true,
       msg:"User Registerd Succesfull",
@@ -51,16 +57,34 @@ const loginConrtoller =async (req, res)=>{
           })
         }
 
-        const user = await User.findOne({email: email, password:password})
+        const user = await User.findOne({email})
          if (!user) {
           return res.status(500).send({
             success: false,
-            msg:"User Not Found Or Password incorrect"
+            msg:"User Not Found"
           })
          }
+         // check Password correct
+         const isMatch = await bcryptjs.compare(password,user.password)
+         if (!isMatch) {
+           return res.status(500).send({
+              success: false,
+              msg:"Password Incorrect"
+             })
+         }
+         // creating Token
+
+         const token = jwt.sign({id:user._id}, process.env.JWT_SECRET, {
+           expiresIn: '7d'
+         })
+         user.password = undefined
          res.status(200).send({
           success: true,
-          msg:'Login Succesfully'
+          msg:'Login Succesfully',
+          token,
+          user,
+          
+
          })
         
        } catch (error) {
