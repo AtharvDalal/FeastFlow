1d29555997c2cd4b848a30be7249c55c4c6e77d1
ATHARV DALAL <sangamdalal24@gmail.com>
2024-02-05 23:11:00 +0530

    Added Routes

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 import express from "express";
-import cors from 'cors'
+import cors from "cors";
 import morgan from "morgan";
 import connectDB from "./db/db.js";
 import router from "./routes/authRoutes.js";
-import userRoute from './routes/userRoute.js'
-import resturantRoute from './routes/restaurantRoutes.js'
-import dotenv from 'dotenv'
-import categoryRoute from './routes/categoryRoute.js'
-const app = express()
+import userRoute from "./routes/userRoute.js";
+import resturantRoute from "./routes/restaurantRoutes.js";
+import dotenv from "dotenv";
+import categoryRoute from "./routes/categoryRoute.js";
+import foodRoute from "./routes/foodRoute.js";
+const app = express();
 
 const PORT = 8000;
- 
-connectDB()
 
-dotenv.config()
+connectDB();
 
-app.use(morgan('dev'))
+dotenv.config();
+
+app.use(morgan("dev"));
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
+app.use("/api/v1/auth", router);
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/resturant", resturantRoute);
+app.use("/api/v1/category", categoryRoute);
+app.use("/api/v1/food", foodRoute);
 
-app.use('/api/v1/auth', router)
-app.use('/api/v1/user', userRoute)
-app.use('/api/v1/resturant',resturantRoute)
-app.use('/api/v1/category', categoryRoute)
-
-app.listen(PORT, ()=>{
-    return console.log(`server was running on PORT:-${PORT}`);
-})
-
+app.listen(PORT, () => {
+  return console.log(`server was running on PORT:-${PORT}`);
+});
