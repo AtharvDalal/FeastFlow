5d6aab7df574800ac913e0b7156a5bf35e58ddec
ATHARV DALAL <sangamdalal24@gmail.com>
2024-02-05 15:21:03 +0530

    some issue in auth middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,31 +1,26 @@
 import jwt from "jsonwebtoken";
 
-
-
-
-
- const authMiddleware =async(req,res,next)=> {
-      
-    try {
-        const token = req.headers["authorization"].split(" ")[1]
-        jwt.verify(token, process.env.JWT_SECRET,(err,decode)=>{
-            if (err) {
-                return res.status(401).send({
-                    success:false,
-                    msg:"Un-Authorize User"
-                })
-            }else{
-                req.body.id = decode.id;
-                next()
-            }
-        })
-    } catch (error) {
-       console.log(error);
-       res.status(500).send({
-        success:false,
-        msg:'Error im auth API'
-       })
-    }
-}
+const authMiddleware = async (req, res, next) => {
+  try {
+    const token = req.headers["authorization"].split(" ")[1];
+    jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
+      if (err) {
+        return res.status(401).send({
+          success: false,
+          msg: "Un-Authorize User",
+        });
+      } else {
+        req.body.id = decode.id;
+        next();
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      msg: "Error in auth API",
+    });
+  }
+};
 
 export default authMiddleware;
