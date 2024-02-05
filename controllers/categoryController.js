f692cad9000a575f3ba129ca0937deb5a4ee5338
ATHARV DALAL <sangamdalal24@gmail.com>
2024-02-05 22:22:45 +0530

    Delete Updated Category Controller Created

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -30,3 +30,87 @@ export const createCategoryController = async(req,res)=>{
           });
           }
 }
+
+export const getAllCategoryController = async(req,res)=>{
+     try {
+       const categories = await Category.find({})
+       if (!categories) {
+        return res.status(404).send({
+          success: false,
+          msg: "No Categories Found"
+        })
+       }
+
+       res.status(200).send({
+        success: true,
+        totalCat: categories.length,
+        categories
+
+       })
+     } catch (error) {
+      console.log(error);
+      res.status(500).send({
+        success: false,
+        msg: "Error In Get All Category  API",
+        error,
+      });
+     }
+}
+
+export const UpdateCategoryController = async(req,res)=>{
+          try {
+            const {id} = req.params;
+            const {title, imageUrl} = req.body;
+
+            const updateCategory = await Category.findByIdAndUpdate(id, {title,imageUrl}, {new: true})
+            if (!updateCategory) {
+               return res.status(500).send({
+                success: false,
+                msg:"No Category Found"
+               })
+            }
+            res.status(200).send({
+              success:true,
+              msg:"Category Updated Successfully"
+            })
+          } catch (error) {
+            console.log(error);
+      res.status(500).send({
+        success: false,
+        msg: "Error In Update Category  API",
+        error,
+      });
+          }
+}
+
+export const deleteCategoryController = async(req,res)=>{
+        try {
+          const {id}= req.params;
+          if(!id){
+            return res.status(500).send({
+              success: false,
+              msg:"Please Provide Valid Category ID"
+            })
+          }
+          const category = await Category.findById(id)
+          if (!category) {
+            return res.status(200).send({
+               success:false,
+               msg:"No Category Found On This ID"
+            })
+          }
+
+          await Category.findByIdAndDelete(id)
+          res.status(200).send({
+            success: true,
+            msg:"Category Deleted SuccessFully"
+          })
+        } catch (error) {
+          console.log(error);
+      res.status(500).send({
+        success: false,
+        msg: "Error In Delete Category  API",
+        error,
+      });
+        }
+}
