e30adaea8c515aefb38051514b226a182c1a7b71
ATHARV DALAL <sangamdalal24@gmail.com>
2024-02-05 22:23:16 +0530

    Updated Category Routes

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,9 +1,17 @@
 import express from 'express'
-import { createCategoryController } from '../controllers/categoryController.js'
+import { UpdateCategoryController, createCategoryController, deleteCategoryController, getAllCategoryController } from '../controllers/categoryController.js'
 
 const router = express.Router()
 
+//Create Category
 router.post('/create', createCategoryController)
 
+// Get ALL Category
+router.get('/getall',getAllCategoryController)
 
+// Update Categories
+router.put('/update/:id',UpdateCategoryController)
+
+//Delete Categories
+router.delete('/delete/:id',deleteCategoryController)
 export default router
