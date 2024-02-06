b5b6a0b08557ce6e448678c0d3fa23cda95e1148
ATHARV DALAL <sangamdalal24@gmail.com>
2024-02-06 14:05:34 +0530

    Get Food By Id

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -48,3 +48,95 @@ export const createFoodController = async (req, res) => {
     });
   }
 };
+
+export const getallFoodController = async()=>{
+  try {
+    const getallFood = await Foods.find({})
+    if (!getallFood) {
+      return res.status(404).send({
+        success:false,
+        msg: "No food Item Was Found"
+      })
+    }
+    res.status(200).send({
+      success:true,
+      totalcount: getallFood.length
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      msg: "Error In Get All Food  API",
+      error,
+    });
+  }
+}
+
+export const getFoodbyIdController = async(req,res)=>{
+  try {
+    const id = req.params.id;
+    if (!id) {
+      return res.status(500).send({
+        success:false,
+        msg:"Id Is Inavlid "
+      })
+    }
+
+  const food = await Foods.findById(id)
+  if (!food) {
+    return res.status(500).send({
+      success:false,
+      msg:"No Food Item Was Found on This ID  "
+    })
+  }
+    res.status(200).send({
+      success:true,
+     food,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      msg: "Error In Get Food By ID  API",
+      error,
+    });
+  }
+}
+
+
+
+
+
+// get Food  By resturant
+export const getFoodbyRestuarantController = async(req,res)=>{
+  try {
+    const resturantid = req.params.id;
+    if (!resturantid) {
+      return res.status(500).send({
+        success:false,
+        msg:"Resturant Id Is Invalid"
+      })
+    }
+
+  const food = await Foods.find({resturant:resturantid})
+  if (!food) {
+    return res.status(500).send({
+      success:false,
+      msg:"No Food Item Was Found on This ID  "
+    })
+  }
+    res.status(200).send({
+      success:true,
+      msg:"food based On Resturant",
+     food,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      msg: "Error In Get Food By ID  API",
+      error,
+    });
+  }
+}
+
--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -1,8 +1,18 @@
 import express from 'express'
-import { createFoodController } from '../controllers/foodController.js'
+import { createFoodController, getFoodbyIdController, getallFoodController } from '../controllers/foodController.js'
 
 const router = express.Router()
 
+// Create Food
 router.post('/create',createFoodController)
 
+
+//Get All Foods
+router.get('/getall',getallFoodController)
+
+//get Food By ID
+router.get('/getone/:id', getFoodbyIdController)
+
+//get Food By Resturant
+router.get('/getbyrest/:id', getFoodbyIdController)
 export default router
